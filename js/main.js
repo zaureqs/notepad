6d29fe4b650925dc875ba24ea0a22fedb4724d4b
zaureqs <zaureqs01@gmail.com>
2022-10-01 17:55:50 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import NotesAPI from "./NotesAPI.js";
+
+// NotesAPI.saveNote({
+//     title: "new note !",
+//     body: "new note body andsfjdkljfl jsldjf jlkfj asl "
+// });
+// NotesAPI.deleteNote(3);
+
+
+console.log(NotesAPI.getAllNotes());
